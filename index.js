ae9e567ab06228df828cd8e68b2daeb4af1f356d
rnrabi <rnrabi913@gmail.com>
2025-01-11 19:40:14 +0600

    ongodb set up ok

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 dotenv.config();
@@ -12,13 +13,43 @@ app.use(cors());
 app.use(bodyParser.json());
 
 
+const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.rjjtc94.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // await client.connect();
+
+
+
+
+
+
+
+     
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 
 
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World CraftMyPlate server... !')
+    res.send('Hello CraftMyPlate server running... !')
 })
 
 app.listen(port, () => {
